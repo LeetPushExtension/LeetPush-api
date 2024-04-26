de01c5517916be7ce7e09d7fa0a66c039a4f77c8
husamahmud <husamahmud@gmail.com>
2024-04-27 02:13:49 +0300

    [feat]: Add limiter middleware

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,26 @@
 import express from 'express'
 import morgan from 'morgan'
+import { rateLimit } from 'express-rate-limit'
 
 import router from './router'
 
 const app = express()
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   limit: 15
+// })
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// app.use(limiter)
 
 app.get('/', (req, res) => {
   res.status(200)
   res.json({
-    message: 'Hello From LeetPush API Server'
-    // TODO: Provide endpoints examples here
+    message: 'LeetPush API',
+    totalSolvedProblems: '/:userId',
+    dailyProblem: '/daily'
   })
 })
 
